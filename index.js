18c6e128cd28f0eb8edaea5b100d36ea2c2d7723
ahsinrasheed <ahsinbhatti5@gmail.com>
2022-06-08 10:24:58 +0500

    creating the new promise and creating the readFilePro function

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
+const readFilePro = file => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, (err, data) => {
+            if (err) reject('I could not find that file');
+            resolve(data);
+        });
+    });
+};
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+readFilePro(`${__dirname}/dog.txt`).then(data => {
     console.log(`Breed :${data}`);
 
     superagent
         .get(`https://dog.ceo/api/breed/${data}/images/random`)
-        .end((err, res) => {
-            if (err) return console.log(err.message);
-            console.log(res.body.message);
+        .then(res => {
+            console.log(res.body.message);  
        
             fs.writeFile('dog-img.txt', res.body.message, err => {
                 if (err) return console.log(err.message);
                 console.log(`Random dog image saved to file...`)
-            });
+            });   
+        })
+        .catch(err => {
+            console.log(err.message);
     });
-});
+})
+
 
